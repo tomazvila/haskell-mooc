9092698031ccce826671691d5de8c8644b1352a0
tomazvila <tomazvila@outlook.com>
2021-11-08 23:56:05 +0200

    maxBy function solution not tested

--- exercises/Set3a.hs
+++ exercises/Set3a.hs
@@ -29,8 +29,13 @@ import Data.List
 --  maxBy head   [1,2,3] [4,5]  ==>  [4,5]
 
 maxBy :: (a -> Int) -> a -> a -> a
-maxBy measure a b = todo
-
+maxBy measure a b
+ | a' > b'   = a
+ | b' > a'   = b
+ | otherwise = a
+  where
+   a' = measure a
+   b' = measure b
 ------------------------------------------------------------------------------
 -- Ex 2: implement the function mapMaybe that takes a function and a
 -- Maybe value. If the value is Nothing, it returns Nothing. If it is
